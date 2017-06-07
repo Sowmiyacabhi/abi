00ad7eb42e961bb38b2a507b75c4957c961fb910
Sowmiyacabhi <sowmiyaec1997@gmail.com>
2017-06-07 17:30:21 +0530

    Create Sign.java

---
+++ Sign.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Sign
+{
+public static void main(String  args[])
+{
+Scanner inp=new Scanner(System.in);
+int n=inp.nextInt();
+if(a>=0)
+{
+if(a==0)
+{
+System.out.println("zero");
+}
+else
+{
+System.out.println("positive");
+}
+}
+else
+{
+System.out.println("negative");
+}
+}
+}
